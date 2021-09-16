11ab683e03dbd158c488a2a0ae8b520f8e5299e1
Arasi Arumugam <ezhil.arum@gmail.com>
2021-09-15 19:03:56 -0500

    Exercise for chapter model binding completed

--- src/main/java/org/launchcode/codingevents/data/EventData.java
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -32,4 +32,12 @@ public class EventData {
         events.remove(id);
     }
 
+//    //update an event
+//
+//    public static void editEvent(int id, String name, String description){
+//        Event eventToedit = events.get(id);
+//        eventToedit.setName(name);
+//        eventToedit.setDescription(description);
+//    }
+
 }
