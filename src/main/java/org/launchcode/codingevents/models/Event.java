57ad11f4566255117a048f7fd44ffcc4180d57be
Arasi Arumugam <ezhil.arum@gmail.com>
2021-09-22 20:51:41 -0500

    Validation exercisepractice completed

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,17 +1,32 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId =1;
 
+    @Size(min=3, max=50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank(message = "Name is required.")
     private String name;
+
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
-    public Event(String name,String description) {
+    @NotBlank(message="Location cannot be left blank.")
+    private String location;
+
+    @Positive(message="Number of attendees must be one or more.")
+    private int numberOfAttendees;
+
+    public Event(String name,String description,String location,int numberOfAttendees) {
         this.name = name;
         this.description= description;
+        this.location=location;
+        this.numberOfAttendees =numberOfAttendees;
         this.id= nextId;
         nextId++;
     }
@@ -34,6 +49,22 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
     }
 
     @Override
